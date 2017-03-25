6ddf5871f8a74c0cfc2b95289954245252c49d0c
davidtsaksomo <davidtheoxcalibur@gmail.com>
2017-03-25 18:42:00 +0700

    Cellv1
    Cell baru kecompile belum ditest

---
+++ Cell.java
@@ -0,0 +1,19 @@
+public abstract class Cell implements Renderable {
+  protected String cell_type;
+  
+  public Cell(String tipe) {
+    cell_type = tipe;
+  }
+  public String GetCellType() {
+    return cell_type;
+  }
+  public abstract String GetFacHabType();  
+  public void SetCellType(String tipe) {
+    cell_type = tipe; 
+  }
+  public abstract void Render(boolean oncage);
+  public abstract void Render();
+  public abstract void Enter();
+  
+
+}
