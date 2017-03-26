5e23f3c4ebabe827e9aaa5484e8dc1cb36172d2f
radiyyadwi <radiyyasaputra@gmail.com>
2017-03-26 11:12:39 +0700

    Habitat
    sudah ke compile

---
+++ Habitat.java
@@ -0,0 +1,39 @@
+public class Habitat extends Cell {
+	protected final String habitat_type;
+
+	public Habitat(String s) {
+		super("Habitat");		
+		habitat_type=s;
+	}
+	public String GetFacHabType() {
+		return habitat_type;
+	}
+	public void Render() {
+		if(habitat_type == "Water") {
+			System.out.print ("w");
+		} else
+		if(habitat_type == "Land") {
+			System.out.print ("l");
+		} else
+		if(habitat_type == "Air") {
+			System.out.print ("a");
+		}
+	}
+	public void Render(boolean oncage) {
+		if(oncage) {
+			if(habitat_type == "Water") {
+				System.out.print ("w");
+			} else
+			if(habitat_type == "Land") {
+				System.out.print ("l");
+			} else
+			if(habitat_type == "Air") {
+				System.out.print ("a");
+		} else {
+			Render();
+		}
+		}
+	}
+	public void Enter() {
+	}
+}
