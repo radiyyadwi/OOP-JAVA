bdc1962c868e1a1008908bc1dbe41627e6e7580d
davidtsaksomo <davidtheoxcalibur@gmail.com>
2017-03-27 20:32:27 +0700

    VZ03Packages
    benerin yg codot

--- VZ03/virtualzoo/cell/habitat/airhabitat/AirHabitat.java
+++ VZ03/virtualzoo/cell/habitat/airhabitat/AirHabitat.java
@@ -1,14 +1,9 @@
 // File : AirHabitat.java
 // Author : Prama Legawa Halqavi (13515132)
- /**
-  * Kelas yang menunjukkan habitat udara
-  * @author Prama Legawa Halqavi/13515132
-  * @version 1.0, March 2017
-  */
+package virtualzoo.cell.habitat.airhabitat;
+import virtualzoo.cell.habitat.Habitat;
+
 public class AirHabitat extends Habitat {
-  /**Constructor AirHabitat
-    *@see java.lang.reflect.Constructor
-	  */
   public AirHabitat() {
     super("Air");
   }
